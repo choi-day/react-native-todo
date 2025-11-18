53afa9c7004842c546fa45dd8553cbffc062986e
choi-day <choisschoco@gmail.com>
2025-11-18 13:49:37 +0900

    상단바 수정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import { StatusBar } from "react-native";
 import styled, {ThemeProvider} from 'styled-components/native'
 import {theme} from './theme';
 
-const Container = styled.View`
+const Container = styled.SafeAreaView`
     flex: 1;
     background-color: ${({theme}) => theme.background};
     align-items: center;
@@ -14,15 +15,19 @@ const Title = styled.Text`
     font-weight: 600;
     color: ${({theme}) => theme.main};
     align-self: flex-start;
-    margin: 0px 20px;
+    margin: 20px;
 `;
 
 export default function App() {
     return (
         <ThemeProvider theme={theme}>
             <Container>
+                <StatusBar
+                barStyle="light-content"
+                backgroundColor={theme.background}
+                />
                 <Title>TODO List</Title>
             </Container>
         </ThemeProvider>
-    )
+    );
 }
