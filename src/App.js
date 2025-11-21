08c7ce139a253fbe477b91754547e12a1fe9e37b
choi-day <choisschoco@gmail.com>
2025-11-21 21:20:13 +0900

    데이터 저장하기

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import { Dimensions, StatusBar } from "react-native";
 import styled, {ThemeProvider} from 'styled-components/native'
 import {theme} from './theme';
 import Input from './components/Input'
-import {images} from './images'
-import IconButton from "./components/IconButton";
 import Task from './components/Task';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Container = styled.SafeAreaView`
     flex: 1;
@@ -29,14 +28,20 @@ const List = styled.ScrollView`
 
 export default function App() {
     const width = Dimensions.get('window').width;
+    
 
     const [newTask, setNewTask] = useState('');
-    const [tasks, setTasks] = useState({
-        '1': { id: '1', text: 'Hanbit' , completed: false },
-        '2': { id: '2' , text: 'React Native', completed: true },
-        '3': { id: '3', text: 'React Native Sample', completed: false },
-        '4': { id: '4', text: 'Edit TODO Item', completed: false },
-    });
+    const [tasks, setTasks] = useState({});
+
+    const _saveTasks = async tasks => {
+        try {
+        await AsyncStorage.setItem('tasks', JSON.stringify(tasks));
+        setTasks(tasks);
+        } catch (e) {
+        console.
+        error(e);
+        }
+    }
 
     const _addTask = () => {
         const ID = Date.now().toString();
@@ -45,21 +50,22 @@ export default function App() {
         };
         setNewTask('');
         setTasks({ ...tasks, ...newTaskObject });
+        _saveTasks({ ...tasks, ...newTaskObject });
     };
     const _deleteTask = id => {
         const currentTasks = Object.assign({}, tasks);
         delete currentTasks[id];
-        setTasks(currentTasks);
+        _saveTasks(currentTasks);
     };
     const _toggleTask = id => {
         const currentTasks = Object.assign({}, tasks);
         currentTasks[id]['completed'] = !currentTasks[id]['completed'];
-        setTasks(currentTasks);
+        _saveTasks(currentTasks);
     };
     const _updateTask = item => {
         const currentTasks = Object.assign({}, tasks);
         currentTasks[item.id] = item;
-        setTasks(currentTasks);
+        _saveTasks(currentTasks);
     };
 
     const _handleTextChange = text => {
